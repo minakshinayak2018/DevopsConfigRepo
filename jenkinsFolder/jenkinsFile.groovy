a5f3fe7f1a68f041f0cf1cea6758853c0950edb4
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-25 18:17:45 +0530

    Adding email notification

--- jenkinsFolder/jenkinsFile.groovy
+++ jenkinsFolder/jenkinsFile.groovy
@@ -74,17 +74,23 @@ pipeline {
 				     	}	
                                }
                        }
-		def("EMAIL NOTIFICATION")
+	         stage(" EMAIL NOTIFICATION") {
+            	           steps {
+                	script {
                     	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
                         def summary = "${subject} (${env.BUILD_URL})"
                         def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-                      <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
-                      <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
-               emailext (
-                subject: subject,
-                body: details,
-                to: commonProps.recipients
-                        )
+                        <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+                        <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+                      emailext (
+                              subject: subject,
+                              body: details,
+                              to: commonProp.recipients
+                              )
+			  }
+                	
+            	    }
+      	     }
 	       echo "EMAIL NOTIFICATION SUCCESS"
                  		
             }
