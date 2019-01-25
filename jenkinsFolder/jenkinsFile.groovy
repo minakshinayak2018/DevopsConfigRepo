96a6c30380bd0dd84245e27ad2a16a6802d1eb56
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-25 23:41:30 +0530

    fix

--- jenkinsFolder/jenkinsFile.groovy
+++ jenkinsFolder/jenkinsFile.groovy
@@ -72,19 +72,20 @@ pipeline {
 				     	}	
                                }
                        }
-		stage('Sending email'){
-		     steps {
-			     script {
-                              try{
-                                sh emailNotification.triggerEmail();
-                               } catch (e) {
-                             currentBuild.result = "FAILED"
-                            sh emailNotification.triggerEmail();
-                         throw e
-                         }
-	                 }
-		     }
-                }
+		def("EMAIL NOTIFICATION")
+		{
+                    	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+                        def summary = "${subject} (${env.BUILD_URL})"
+                        def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+                      <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+                      <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+             emailext (
+                subject: subject,
+                body: details,
+                to: commonProps.recipients
+               )
+	       echo "EMAIL NOTIFICATION SUCCESS"
+                   }
 	                        		
             }
     }
