6d5ad9d4efa05f6d5b98e4d33f9d005525641ec6
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-25 23:38:13 +0530

    Update jenkinsFile.groovy

--- jenkinsFolder/jenkinsFile.groovy
+++ jenkinsFolder/jenkinsFile.groovy
@@ -8,7 +8,7 @@ pipeline {
 			     commonProp = readProperties file:'propertiesFiles/common.properties'
 			     artifactoryProp = readProperties file:'propertiesFiles/artifactory.properties'
 			     deployProps = readProperties file:'propertiesFiles/deploy.properties'
-			     emailNotification = load file:'config/reUsable.groovy'
+			     emailNotification = readProperties file:'config/reUsable.groovy'
                              echo 'LOAD SUCCESS'
                              }
                       }    
