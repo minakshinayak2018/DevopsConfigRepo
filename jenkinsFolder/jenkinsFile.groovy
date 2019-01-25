874efd9df8affbcfef8a10e5bda32bcca6bf949e
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-25 17:42:59 +0530

    email notification

--- jenkinsFolder/jenkinsFile.groovy
+++ jenkinsFolder/jenkinsFile.groovy
@@ -74,5 +74,19 @@ pipeline {
 				     	}	
                                }
                        }
-              }
+		def("EMAIL NOTIFICATION") {
+                    	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+                        def summary = "${subject} (${env.BUILD_URL})"
+                        def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+                      <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
+                      <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
+               emailext (
+                subject: subject,
+                body: details,
+                to: commonProps.recipients
+                        )
+	       echo "EMAIL NOTIFICATION SUCCESS"
+                 }
+		
+            }
     }
