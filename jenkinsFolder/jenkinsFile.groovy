bebf2f49278bb008b1780d6ed23878538fa95fa0
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-25 16:16:29 +0530

    Update jenkinsFile.groovy

--- jenkinsFolder/jenkinsFile.groovy
+++ jenkinsFolder/jenkinsFile.groovy
@@ -48,23 +48,7 @@ pipeline {
 				echo 'UPLOAD ARTIFACT SUCCESS'
 				}
 			}
-	    stage("EMAIL NOTIFICATION") {
-            	steps {
-                	script {
-                    	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-                        def summary = "${subject} (${env.BUILD_URL})"
-                        def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-                      <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
-                      <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
-             emailext (
-                subject: subject,
-                body: details,
-                to: commonProps.recipients
-               )
-	       echo "EMAIL NOTIFICATION SUCCESS"
-           }
-        }
-      }
+	   
 		stage('DEPLOY') {
                   steps {
 		  	script {
