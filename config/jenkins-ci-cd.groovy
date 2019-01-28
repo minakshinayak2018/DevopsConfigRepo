f609ae46ceeb256fbc0d983186f469f3d3c3cfea
minakshinayak2018 <44997746+minakshinayak2018@users.noreply.github.com>
2019-01-28 23:23:55 +0530

    update

--- config/jenkins-ci-cd.groovy
+++ config/jenkins-ci-cd.groovy
@@ -1,3 +1,5 @@
+def utilRepo,commonUtility,
+def credentialsId= 'e67dc0aa-09f5-481f-8980-24c6d8ab3234'
 pipeline {
 	agent any
         	stages {
@@ -8,48 +10,56 @@ pipeline {
 			       commonProps = readProperties file:'./propertiesFiles/common.properties'
 			       artifactoryProps = readProperties file:'./propertiesFiles/artifactory.properties'
 			       deployProps = readProperties file:'./propertiesFiles/deploy.properties'
-			       configProp = load commonProp.configFile
+			       utilRepo =jenkinsGroovy.gitCheckout(gitProp['gitUtilRepo'], gitProp['branchName'],credentialsId);
+			       commonUtility = load "util/commonUtility.groovy"
 			       echo 'LOAD SUCCESS'
 				         }
-			       }
+			            }
 			   }
 			 stage('READ GIT') {
                   steps {
 		  	   script {
-                           git url: gitProp.gitUrl,
+                           git url: gitProp.gitAppRepo,
                            branch: gitProp.branchName
                            echo 'READ SUCCESS'
-				                    }
-					               }
+				               }
+			        }
 				             }
 			      stage('SONAR SCAN') {
                   steps {
 		  	            script {
-			                configProp.scan()
+			                commonUtility.scan(commonProps)
 				             }
 				         }
 			        }
 			      stage('BUILD') {
                   steps {
 		  	               script {
-			                    configProp.build()
+			                    commonUtility.build(commonProps)
 			                      }
 				               }
 			            }
 			      stage('UPLOAD ARTIFACT') {
                              steps {
 		  	                         script {
-			                       configProp.artifactory()
+			                       commonUtility.artifactory(artifactoryProps)
 			                                }
 				                           }
 			              }
 			     stage('DEPLOY') {
                          steps {
 		  	                     script {
-			                        configProp.deploy()
+			                        commonUtility.deploy(deployProps)
 			                       deleteDir()
 			                 }
                 }         
-            }    
+            } 
+			stage('EMAIL NOTIFICATION') {
+				steps{
+					script {
+						commonUtility.notifyBuild(env.BUILD_STATUS)
+					}
+				}
+			}
      	}
 }
